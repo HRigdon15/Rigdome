382f7bd3482be4cd75bfa6762bda228aed0c842e
HRigdon <m225484@usna.edu>
2022-02-13 15:03:36 -0500

    initial_commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,24 @@
 class ProductsController < ApplicationController
   def index
+    @products = Product.all
   end
+  
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(name: "...", description: "...", seller:"...", price:"$...")
+
+    if @product.save
+      redirect_to @product
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
 end
